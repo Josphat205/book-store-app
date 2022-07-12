b83c35bee93576dba2bf5a0d29e5b4e9f436dcb9
lomantech <87063237+lomantech@users.noreply.github.com>
2022-07-12 15:40:43 +0300

    Add configurestore file

---
+++ src/redux/configureStore.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './books/books'
+import categoriesReducer from './categories/categories'
+export const store = configureStore({
+  reducer: {
+    myStore: counterReducer,
+    categories: categoriesReducer
+  },
+})
